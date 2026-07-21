cb9ae298316415e16882a360dc9a7036b6db41ec
Altaf Raja <rajaaltaf8874@gmail.com>
2026-07-21 14:47:31 +0530

    add function

---
+++ LAB1/hello.js
@@ -0,0 +1,18 @@
+function sayhello (name){
+    console.log(`welcome to ${name}`);
+
+}
+const sayHi = (name)=>{
+    console.log(`welcome to $(name)`);
+};
+function sum(a,b){
+    return a+b;
+};
+const sum1=(a,b)=>a+b;
+sayhello("altaf raja");
+sayHi("aman")
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
+console.log(`sum of 2 and 5 is ${sum1(2,5)}`);
+
+
+
