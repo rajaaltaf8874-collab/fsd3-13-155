ed9e92a52f4211663e1dcfaf18cf7fa732c12f3b
Altaf Raja <rajaaltaf8874@gmail.com>
2026-09-16 12:04:15 +0530

    add post request with body

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -7,8 +7,18 @@ const server = http.createServer((req, res) => {
 
     
     } else if (req.url === "/" && req.method === "POST") {
-        res.statusCode = 200;
-        res.end("POST Request");
+        //console.log ("Request:",req);
+        let body = ''
+        req.on('data',(chunk)=>{
+            body +=chunk;
+        })
+        req.on("end",()=>{
+            const product =JSON.parse(body);
+            console.log("received product", product);
+             res.statusCode = 201;
+        res.end(JSON.stringify({msg:'product added',product}));
+        })
+       
     } else if (req.url === "/" && req.method === "PUT") {
         res.statusCode = 200;
         res.end("PUT Request");
